7739ac61f7ac76e92aa05f773f123a4d31c228ed
Sahil Kumar <reavensnape07@gmail.com>
2024-04-22 11:41:45 +0530

    Deployed backend link added

--- src/components/Hero/Container.jsx
+++ src/components/Hero/Container.jsx
@@ -17,7 +17,8 @@ const Container = (Hero) => {
 
         if (lat && lon) {
           const response = await fetch(
-            `http://localhost:5000/api/weather?lat=${lat}&lon=${lon}`
+            // `http://localhost:5000/api/weather?lat=${lat}&lon=${lon}`
+            `https://weather-app-sky-gazer-backend.vercel.app/api/weather?lat=${lat}&lon=${lon}`
           );
           if (response) {
             const responseData = await response.json();
@@ -52,7 +53,7 @@ const Container = (Hero) => {
     const handleSearchOption = (option) => {
       setSearchOption(option);
     };
-    
+
     return (
       <>
         <Hero
